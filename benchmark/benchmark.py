8b54072f057e735f203abf408efd9c6de97839f2
hilbertmgomes <gomes.hilbert@gmail.com>
2026-08-04 20:00:23 -0300

    Testes de plotar grafico de analise assintotica

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -8,26 +8,27 @@ import matplotlib.pyplot as plt
 import time
 import random
 
-def gerar_numero_aleatorio(n_digitos):
-    return random.randint(pow(10, n_digitos-1), pow(10, n_digitos) - 1)
+def gerar_numero_aleatorio(n_bits):
+    return random.getrandbits(n_bits)
 
-
-tamanhos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+tamanhos = [16, 32, 64, 128, 256]
 tempos = []
 
 for tam in tamanhos:
-    n = gerar_numero_aleatorio(tam)
     soma_tempos = 0
 
     for _ in range(1000):
+        n = gerar_numero_aleatorio(tam)
         inicio = time.perf_counter()
-        miller_rabin(n, 20)
+        miller_rabin(n, 10)
         soma_tempos += (time.perf_counter() - inicio)
 
     tempos.append(soma_tempos/1000)
 
 
-plt.plot(tamanhos, tempos)
+plt.plot(tamanhos, tempos, label="Analise Assintótica")
+plt.xlabel("Tamanho")
+plt.ylabel("Tempo")
 plt.show()
 
 
