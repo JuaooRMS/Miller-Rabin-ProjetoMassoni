2d017bf1061ba1704db2aadc9b6e5117cfd9fa82
mateus.cordeiro <msgdhdjf777@gmail.com>
2026-08-04 19:40:15 -0300

    testes
    Criação (Sem implementação) dos arquivos de testes de algoritmo, com algumas ideias iniciais de teste

---
+++ testes/testes_basicos.py
@@ -0,0 +1,9 @@
+from miller_rabin import miller_rabin
+print ("Testes básicos do algoritmo Miller-Rabin") 
+# Casos base
+# Primos pequenos
+# Compostos pequenos
+# Quadrados perfeitos
+# Carmichael
+# Primos grandes
+# Compostos grandes
---
+++ testes/testes_de_estresse.py
@@ -0,0 +1,6 @@
+from miller_rabin import miller_rabin
+print ("Testes de estresse do algoritmo Miller-Rabin")
+# Comparação com sympy.isprime()
+# Números aleatórios de 8 até 4096 bits
+# Estatísticas de acertos
+# Estatísticas de tempo
